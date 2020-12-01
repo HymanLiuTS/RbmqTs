e5fd6880c29ffcf12f8471e8032f8c75954809ff
HymanLiuTS <879651072@qq.com>
2020-12-01 13:44:15 +0800

    提交工程

---
+++ rabbitmq-provider/src/main/java/cn/codenest/rabbitmqprovider/service/OriginalRbmqService.java
@@ -0,0 +1,18 @@
+package cn.codenest.rabbitmqprovider.service;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * <h3>springbootts</h3>
+ * <p></p>
+ *
+ * @author : Hyman
+ * @date : 2020-12-01 12:29
+ **/
+public interface OriginalRbmqService {
+
+    public void sendMsg(String msg);
+
+    public void createDirectExchange() throws IOException, TimeoutException;
+}
---
+++ rabbitmq-provider/src/main/java/cn/codenest/rabbitmqprovider/service/impl/OriginalRbmqServiceImpl.java
@@ -0,0 +1,57 @@
+package cn.codenest.rabbitmqprovider.service.impl;
+
+import cn.codenest.rabbitmqprovider.service.OriginalRbmqService;
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import org.springframework.stereotype.Service;
+
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author ：Hyman
+ * @date ：Created in 2020/12/1 12:31
+ * @description：
+ * @modified By：
+ * @version: $
+ */
+@Service
+public class OriginalRbmqServiceImpl implements OriginalRbmqService {
+
+    private String rabbitMqHost = "127.0.0.1";
+    private String rabbitMqUsername = "guest";
+    private String rabbitMqPassword = "guest";
+    private Integer rabbitMqPort = 5672;
+    private Integer rabbitMqTimeOut = 10;
+
+    @Override
+    public void sendMsg(String msg) {
+
+    }
+
+    @Override
+    public void createDirectExchange() throws IOException, TimeoutException {
+        // 创建连接和信道
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost(this.rabbitMqHost);
+        factory.setPort(this.rabbitMqPort);
+        factory.setConnectionTimeout(this.rabbitMqTimeOut);
+        factory.setUsername(this.rabbitMqUsername);
+        factory.setPassword(this.rabbitMqPassword);
+        Connection connection = null;
+        try {
+            connection = factory.newConnection();
+            Channel channel= connection.createChannel();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (TimeoutException e) {
+            e.printStackTrace();
+        } finally {
+            if (connection != null && connection.isOpen()) {
+                connection.close();
+            }
+        }
+
+    }
+}
